f9340a3d932f23e0835a83d72dcbc4de25ad9705
Chris Nader <chrisnader25@gmail.com>
2026-07-15 11:05:36 +0300

    Conversion var to byte exception using try and catch

---
+++ VarToByteExceptionProgram.cs
@@ -0,0 +1,9 @@
+﻿try
+{
+    var s = "1234";
+    byte b = Convert.ToByte(s);
+    Console.Write("Converting from \"1234\" to byte worked.");
+}
+catch (Exception){
+    Console.WriteLine("Converting from \"1234\" to byte did not worked.");
+}
