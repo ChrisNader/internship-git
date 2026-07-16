41866c9522cc201321f88907f12f70bc8bbd1645
Chris Nader <chrisnader25@gmail.com>
2026-07-16 10:33:57 +0300

    More of data type conversions

--- DataTypeConversionProgram.cs
+++ DataTypeConversionProgram.cs
@@ -1,22 +1,19 @@
 ﻿byte b = 3;
 int i = b;
 
-Console.Write("1. From byte to integer does not need a special function: ");
-Console.WriteLine(i);
+Console.WriteLine("1. From byte to integer does not need a special function: " + i);
 
 i = 3;
-b = (byte) i;
+b = (byte)i;
 
-Console.Write("2. From integer to byte using casting: ");
-Console.WriteLine(i);
+Console.WriteLine("2. From integer to byte using casting: " + i);
 
 i = 1000;
 b = (byte)i;
 
 Console.WriteLine("3. byte is limited from 0 to 255");
 Console.WriteLine("\tex: having i=1000");
-Console.Write("\tconverting it to byte gives us: ");
-Console.WriteLine(b);
+Console.WriteLine("\tconverting it to byte gives us: " + b);
 Console.WriteLine("\tbecause some bits are lost.\n");
 
 
@@ -26,9 +23,26 @@ i = Convert.ToInt32(s);
 int p = int.Parse(s);
 
 Console.WriteLine("\n4. Converting from a string to integer:");
-Console.Write("- using Convert.ToInt32: ");
-Console.WriteLine(i);
-Console.Write("- using int.Parse: ");
-Console.WriteLine(p);
-Console.WriteLine("and the same goes with different Converto.To* functions.");
+Console.WriteLine("\t- using Convert.ToInt32: " + i);
+Console.WriteLine("\t- using int.Parse: " + p);
+Console.WriteLine("*And the same goes with different Converto.To* functions.");
 
+i = 321;
+s = Convert.ToString(i);
+Console.WriteLine("5. Converting from integer to string: " + i);
+
+double d = 3.14;
+i = Convert.ToInt32(d);
+Console.WriteLine("6. Converting from double to integer: " + i);
+
+i = 123;
+d = Convert.ToDouble(i);
+Console.WriteLine("7. Converting from integer to double: " + d);
+
+s = "$";
+char c = Convert.ToChar(s);
+Console.WriteLine("8. Converting from string to character: " + c);
+
+s = "true";
+bool bb = Convert.ToBoolean(s);
+Console.WriteLine("9. Converting from string to boolean: " + bb);
