0c714cef11a3f5af148620c644cb1044401af9b6
Chris Nader <chrisnader25@gmail.com>
2026-07-20 12:43:14 +0300

    Days switches

---
+++ daysSwitchesProgram.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("What day is it today?");
+String day = Console.ReadLine();
+day = day.ToLower();
+
+switch (day)
+{
+    case "":
+        Console.WriteLine("You did not enter a day!");
+        break;
+    case "monday":
+        Console.WriteLine("It's Monday!");
+        break;
+    case "tuesday":
+        Console.WriteLine("It's Tuesday!");
+        break;
+    case "wednesday":
+        Console.WriteLine("It's Wednesday!");
+        break;
+    case "thursday":
+        Console.WriteLine("It's Thursday!");
+        break;
+    case "friday":
+        Console.WriteLine("It's Friday!");
+        break;
+    case "saturday":
+        Console.WriteLine("It's Saturday!");
+        break;
+    case "sunday":
+        Console.WriteLine("It's Sunday!");
+        break;
+    default:
+        Console.WriteLine(day + " is not a day!");
+        break;
+}
