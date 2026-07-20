02c313f631c76fa68f26611dc6bc74a914bf082b
Chris Nader <chrisnader25@gmail.com>
2026-07-20 13:06:19 +0300

    Generating a table of a symbol using nested for loops

---
+++ TableSymbolProgram.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("How many columns do you need? ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("\nHow many rows do you need? ");
+int r = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("\nEnter a symbol: ");
+char symbol = (char)Console.Read();
+
+Console.WriteLine();
+
+for (int i = 0; i < r; i++)
+{
+    for (int j = 0; j < c; j++)
+    {
+        Console.Write(symbol);
+    }
+    Console.WriteLine();
+}
