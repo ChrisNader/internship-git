54c1d0e70cf88f103acb431497c9cf52620689a8
Chris Nader <chrisnader25@gmail.com>
2026-07-20 12:34:42 +0300

    Name and age checker using if statements

---
+++ ifStatementsProgram.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Please enter your name: ");
+String name = Console.ReadLine();
+
+if (name == "") Console.WriteLine("You did not enter your name!");
+else
+{
+    Console.WriteLine("Hello " + name);
+
+    Console.WriteLine("Please enter your age: ");
+    int age = Convert.ToInt32(Console.ReadLine());
+
+    if (age < 18) Console.WriteLine("You have to be +18 to sign up.");
+    else if (age > 100) Console.WriteLine("You are too old to sign up");
+    else Console.WriteLine("You are signed up");
+}
